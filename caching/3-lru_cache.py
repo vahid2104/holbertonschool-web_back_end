febfb37102da7cfffa269bcf9eade33826f30fe2
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2025-11-15 09:27:38 +0100

    Update 3-lru_cache.py

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python3
+"""LRU caching module.
+This module defines the LRUCache class that implements
+a Least Recently Used (LRU) caching system.
+"""
 
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """LRUCache class that inherits from BaseCaching
+    Implements a caching system using Least Recently Used (LRU) algorithm.
+    """
+
     def __init__(self):
+        """Initialize the LRUCache."""
         super().__init__()
-        self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """Add an item in the cache.
+
+        Args:
+            key (str): The key under which the item is stored.
+            item: The value to store in the cache.
+        """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            self.cache_data[key] = item
-            if key in self.order:
-                self.order.remove(key)
-            self.order.append(key)
-            return
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lru_key = self.order.pop(0)
-            del self.cache_data[lru_key]
-            print("DISCARD: {}".format(lru_key))
-
+            # Move key to end to mark it as recently used
+            self.cache_data.move_to_end(key)
         self.cache_data[key] = item
-        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Pop the first item (least recently used)
+            discarded_key, _ = self.cache_data.popitem(last=False)
+            print("DISCARD: {}".format(discarded_key))
 
     def get(self, key):
+        """Retrieve an item from the cache.
+
+        Args:
+            key (str): The key of the item to retrieve.
+
+        Returns:
+            The value associated with the key if it exists, else None.
+        """
         if key is None or key not in self.cache_data:
             return None
 
-        self.order.remove(key)
-        self.order.append(key)
-        return self.cache_data.get(key)
+        # Move key to end to mark it as recently used
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
