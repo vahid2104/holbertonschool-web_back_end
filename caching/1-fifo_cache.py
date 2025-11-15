fdd1e77493d47bfad1d5360ce4681578562f5a10
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2025-11-15 08:45:19 +0100

    Refactor put method to update key order in FIFO cache
    Updated put method to handle key reordering in FIFO cache.

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -9,22 +9,26 @@ class FIFOCache(BaseCaching):
         self.order = []
 
     def put(self, key, item):
-        if key is None or item is None:
-            return
+    if key is None or item is None:
+        return
 
-        if key in self.cache_data:
-            self.cache_data[key] = item
-            return
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            fifo_key = self.order.pop(0)
-            del self.cache_data[fifo_key]
-            print("DISCARD: {}".format(fifo_key))
-
+    if key in self.cache_data:
         self.cache_data[key] = item
+        self.order.remove(key)
         self.order.append(key)
+        return
+
+    if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        fifo_key = self.order.pop(0)
+        del self.cache_data[fifo_key]
+        print("DISCARD: {}".format(fifo_key))
+
+    self.cache_data[key] = item
+    self.order.append(key)
+
 
     def get(self, key):
         if key is None:
             return None
         return self.cache_data.get(key)
+
