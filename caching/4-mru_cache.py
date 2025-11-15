28806d8d0b0ff8e4a2a6753b1e5efcbdeb4f89c5
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2025-11-15 09:32:05 +0100

    Update 4-mru_cache.py

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -8,45 +8,44 @@ from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """
-    MRUCache class that inherits from BaseCaching
-    Implements a Most Recently Used (MRU) caching system
+    MRUCache class inherits from BaseCaching
+    Implements Most Recently Used (MRU) caching
     """
 
     def __init__(self):
-        """Initialize the MRUCache with an order list"""
+        """Initialize MRUCache with order tracking"""
         super().__init__()
         self.order = []
 
     def put(self, key, item):
         """
-        Add an item in the cache
-        If the number of items exceeds BaseCaching.MAX_ITEMS,
-        discard the most recently used (MRU) item
+        Add an item in cache_data.
+        If cache exceeds MAX_ITEMS, discard the most recently used (MRU) item.
         """
         if key is None or item is None:
             return
 
         if key in self.cache_data:
-            # Update the value and mark as most recently used
+            # Update value and mark as MRU
             self.cache_data[key] = item
-            if key in self.order:
-                self.order.remove(key)
+            self.order.remove(key)
             self.order.append(key)
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Remove the most recently used item
+            # Discard MRU item
             mru_key = self.order.pop(-1)
             del self.cache_data[mru_key]
-            print("DISCARD: {}".format(mru_key))
+            print(f"DISCARD: {mru_key}")
 
         self.cache_data[key] = item
         self.order.append(key)
 
     def get(self, key):
         """
-        Return the value linked to key
-        Return None if key is None or doesn't exist
+        Return value linked to key.
+        Return None if key is None or doesn't exist.
+        Does not affect MRU order.
         """
         if key is None or key not in self.cache_data:
             return None
