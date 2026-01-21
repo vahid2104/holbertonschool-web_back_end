e1b65c14c666c8f40c0357e653a59fee1c07ca0f
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-21 17:18:36 +0100

    Remove 404 error handler and add 401 handler

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,18 +14,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+@app.errorhandler(401)
+def unauthorized(error):
+    """Unauthorized error handler"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-@app.errorhandler(401)
-def unauthorized(error):
-    """Unauthorized error handler"""
-    return jsonify({"error": "Unauthorized"}), 401
+
