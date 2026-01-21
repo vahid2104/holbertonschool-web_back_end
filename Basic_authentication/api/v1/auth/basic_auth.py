42ae0e052509e4dee213da45def497d74f72d3ca
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-21 17:49:45 +0100

    Update basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-"""BasicAuth module
-"""
+"""BasicAuth module"""
 
 import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """BasicAuth class
-    """
+    """Basic authentication class"""
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
-        """Extract the Base64 part of the Authorization header
+        """
+        Extracts the Base64 part of the Authorization header
         """
         if authorization_header is None:
             return None
@@ -24,7 +23,8 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
-        """Decode a Base64 string
+        """
+        Decodes a Base64 authorization header
         """
         if base64_authorization_header is None:
             return None
@@ -35,3 +35,18 @@ class BasicAuth(Auth):
             return decoded.decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str):
+        """
+        Extracts user email and password from decoded Base64 value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
