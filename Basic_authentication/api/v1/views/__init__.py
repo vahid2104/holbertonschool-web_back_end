3d83976cb8beaf2bb45f7629feb5f48a18b7a1f1
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-21 17:10:11 +0100

    Add files via upload

---
+++ Basic_authentication/api/v1/views/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Views module"""
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *  # noqa: E402,F401
+from api.v1.views.users import *  # noqa: E402,F401
