767ac0fb0cecd0d29b90f8760c984f1fc057d7c0
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-21 18:12:09 +0100

    Update docstrings for unauthorized and forbidden routes

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -2,7 +2,6 @@
 """Index views
 """
 from flask import jsonify, abort
-
 from api.v1.views import app_views
 
 
@@ -14,11 +13,11 @@ def status():
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
-    """Test 401 error"""
+    """Trigger 401"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden():
-    """Test 403 error"""
+    """Trigger 403"""
     abort(403)
