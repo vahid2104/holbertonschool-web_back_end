e2da6365c8d45e843afffe52947ab504bf9943c5
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-21 17:32:08 +0100

    Refactor require_auth method for authentication checks
    Enhance require_auth method to check for None and handle excluded paths.

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,9 +10,23 @@ User = TypeVar('User')
 class Auth:
     """Auth class"""
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if path requires auth"""
-        return False
+        def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if path requires authentication"""
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """Returns the Authorization header value"""
