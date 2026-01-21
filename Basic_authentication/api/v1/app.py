f92c01cd912c0f97e94486da83e478edc860b91b
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-21 17:14:13 +0100

    Add 401 error handler for unauthorized access

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -25,3 +25,7 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+@app.errorhandler(401)
+def unauthorized(error):
+    """Unauthorized error handler"""
+    return jsonify({"error": "Unauthorized"}), 401
